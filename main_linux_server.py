9636e5539575bb0476d330b1769d42e9ddc889a9
dzmitrynestsiaryk-stack <dzmitry.nestsiaryk@arpa.veneto.it>
2026-06-17 22:53:01 +0200

    234

--- main_linux_server.py
+++ main_linux_server.py
@@ -343,7 +343,6 @@ async def _run_position_reconciliation_periodically(
             sync_worker.run_position_reconciliation()
             sync_worker.run_trade_based_reconciliation()
             sync_worker.run_protective_orders_reconciliation()
-            sync_worker.run_funding_reconciliation()
         except Exception:
             logger.exception("periodic position/tp reconciliation error")
 
@@ -653,7 +652,6 @@ async def _async_main(
                 for worker in (execution_runtime.sync_workers or {}).values():
                     worker.run_reconciliation()
                     worker.run_position_reconciliation()
-                    worker.run_funding_reconciliation()
             except Exception:
                 logger.warning("startup reconciliation failed (non-critical)")
 
